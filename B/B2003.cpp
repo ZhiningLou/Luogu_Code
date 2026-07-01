5c44150228d0694e2eea1469a69128b13756499d
Ken Lou 娄智宁 <zhininglou.lzn@gmail.com>
2026-07-02 04:59:32 +0800

    Update

---
+++ B/B2003.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+long long a,b,c;
+int main()
+{
+    scanf("%lld%lld%lld",&a,&b,&c);
+    printf("%lld",b);
+    return 0;
+}
---
+++ B/B2004.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+long long a,b,c;
+int main()
+{
+    scanf("%lld%lld%lld",&a,&b,&c);
+    printf("%8lld %8lld %8lld",a,b,c);
+    return 0;
+}
